f2fd5513ea7c8e82175f1395ad653eb76dd4e2d5
Bryan Kennedy <plusbryan@gmail.com>
2013-09-14 11:40:20 -0700

    update to 1.6.2: iOS 7 bug fixes

--- ShipLib/ShipLib.framework/Versions/A/Headers/ShipLib.h
+++ ShipLib/ShipLib.framework/Versions/A/Headers/ShipLib.h
@@ -7,8 +7,8 @@
 //
 //  Documentation: https://github.com/sincerely/shiplib-ios-framework
 //
-//  Version 1.6.1
-//  Updated: 9/13/13
+//  Version 1.6.2
+//  Updated: 9/14/13
 
 #import <ShipLib/SincerelyConstants.h>
 #import <ShipLib/SYSincerelyController.h>
